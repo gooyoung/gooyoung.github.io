07d9336c5b36a674db97affb2ae41695eafda432
gooyoung <gooyoung@foxmail.com>
2016-02-23 04:52:02 +0800

    myresume
    我的前端求职简历

--- js/main.js
+++ js/main.js
@@ -1,62 +1,80 @@
+//tooltip微信二维码提示框
+var className = 'tooltip-box';
+function showTooltip(obj, id, html) {
+    if (document.getElementById(id) == null) {
+        var tooltipBox;
+        tooltipBox = document.createElement('div');
+        tooltipBox.className = className;
+        tooltipBox.id = id;
+        tooltipBox.innerHTML = html;
 
+        obj.appendChild(tooltipBox);
 
-//wechat二维码提示框
-	var className = 'tooltip-box';
+        //提示框浮动
+        tooltipBox.style.position = "absolute";
+        tooltipBox.style.display = "block";
 
-	// 创建div节点
-	//obj-超链接元素，id-提示框id，html-提示框内容
-    function showTooltip(obj, id, html) {
-        if (document.getElementById(id) == null) {
-            var tooltipBox;
-            tooltipBox = document.createElement('div');
-            tooltipBox.className = className;
-            tooltipBox.id = id;
-            tooltipBox.innerHTML = html;
-
-            obj.appendChild(tooltipBox);
-
-            //提示框浮动
-            tooltipBox.style.position = "absolute";
-            tooltipBox.style.display = "block";
-
-            //提示框位置
-            var left = obj.offsetLeft-170;
-            var top = obj.offsetTop-100;
+        //提示框位置
+        var left = obj.offsetLeft-170;
+        var top = obj.offsetTop-100;
             
-            tooltipBox.style.left = left + 'px';
-            tooltipBox.style.top = top + 'px';
+        tooltipBox.style.left = left + 'px';
+        tooltipBox.style.top = top + 'px';
             
-            //鼠标指针移除，提示框隐藏display = "none";
-            obj.onmouseleave = function () {
-                setTimeout(function () {
-                    document.getElementById(id).style.display = "none";
-                }, 100);
-            };
+        //鼠标指针移除，提示框隐藏display = "none";
+        obj.onmouseleave = function () {
+            setTimeout(function () {
+                document.getElementById(id).style.display = "none";
+            }, 100);
+        };
 
-        } 
-        else {   
-            //已经存在则显示出来
-            document.getElementById(id).style.display = "block";
-        }
-    }
-
-    var t = document.getElementById("tooltip");
-
-    //鼠标指针进入触发
-    t.onmouseenter = function () {
-        var _html = '<img src="images/WeChat.png" />';
-        showTooltip(this, "t", _html);
-    };
-
-
-window.onorientationchange = function(){
-    switch(window.orientation){
-        case -90:
-        case 90:
-        alert("横屏:" + window.orientation);
-        case 0:
-        case 180:
-        alert("竖屏:" + window.orientation);
-        break;
+    } 
+    else {   
+        document.getElementById(id).style.display = "block";
     }
 }
+
+var t = document.getElementById("tooltip");
+
+t.onmouseenter = function () {
+    var _html = '<img src="images/WeChat.png" />';
+    showTooltip(this, "t", _html);
+};
+
+
+// page1轮播图
+$(function(){
+    // 初始化
+    $(".tu li").first().show();
+    $(".shu li").first().addClass("active");
+        
+    // 手动控制图片的淡入淡出
+    $(".shu li").mouseover(function(){
+        $(this).addClass("active").siblings("li").removeClass("active");
+        var index=$(this).index();
+        i=index
+        $(".tu li").eq(index).fadeIn().siblings().fadeOut();
+    })
+
+    // 通过时间间隔函数，控制图片的显示
+    var i=0;
+    var t=setInterval(move,2000);
+
+    //动画运行的核心函数
+    function move(){
+        i++
+        if(i==5){
+            i=0;
+        }
+        $(".shu li").eq(i).addClass("active").siblings().removeClass("active")
+        $(".tu li").eq(i).fadeIn().siblings().fadeOut();
+    }
+            
+
+    //鼠标移入out元素的适合，停掉正在运行的动画
+    $(".out").hover(function(){
+        clearInterval(t);
+    },function(){
+        t=setInterval(move,2000);   
+    })
+})
